b2c3e423a10a94db380bcf3aca8019fbb4dfffcb
Eddie Rangel <hacker@eddierangel.com>
2017-10-15 11:37:29 -0700

    adding assembly

---
+++ examples/ExampleProgram.s
@@ -0,0 +1,76 @@
+	.file	"ExampleProgram.c"
+	.text
+	.globl	doSomething
+	.type	doSomething, @function
+doSomething:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	-4(%rbp), %eax
+	addl	$1, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	doSomething, .-doSomething
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Please enter an integer value: "
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"Your new value: %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$0, -12(%rbp)
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	__isoc99_scanf
+	movl	-12(%rbp), %eax
+	movl	%eax, %edi
+	call	doSomething
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC2, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	xorq	%fs:40, %rdx
+	je	.L5
+	call	__stack_chk_fail
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.1-2ubuntu1~16.04) 5.4.1 20160904"
+	.section	.note.GNU-stack,"",@progbits
