f0114c8fcb7e39df8226fbcec09250c1df42cc44
Eddie Rangel <hacker@eddierangel.com>
2017-10-17 13:56:26 -0700

    Update Content

---
+++ examples/addition.c
@@ -0,0 +1,24 @@
+/*
+    Eddie Rangel
+    addition.c
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+    int x, y;
+
+    x = 2;
+    y = 2;
+    x = x + 1;
+    y = y - 1;
+    x++;
+    y++;
+    y--;
+
+    printf("%d",x);
+    printf("%d",y);
+
+
+}
